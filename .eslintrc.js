9db3479c5c2cd16be9abeabfd912d26e61b736b4
mango-martin <martin@mango-byte.com>
2022-11-08 22:11:42 +0700

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:solid/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['eslint-plugin-prettier'],
+  rules: {},
+};
